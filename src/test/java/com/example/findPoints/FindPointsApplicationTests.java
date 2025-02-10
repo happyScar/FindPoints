76bab5a7fb212329329f1e5e4a4e6fb8327be13d
zxd <b21032130@njupt.edu.cn>
2025-02-10 10:32:03 +0800

    update:更改测试单点距离方法

--- src/test/java/com/example/findPoints/FindPointsApplicationTests.java
+++ src/test/java/com/example/findPoints/FindPointsApplicationTests.java
@@ -1,30 +1,31 @@
 package com.example.findPoints;
 
-import org.gavaghan.geodesy.Ellipsoid;
-import org.gavaghan.geodesy.GeodeticCalculator;
-import org.gavaghan.geodesy.GeodeticCurve;
-import org.gavaghan.geodesy.GlobalCoordinates;
+import com.example.findPoints.info.PointInfo;
+import com.example.findPoints.utils.CoordinateUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
 class FindPointsApplicationTests {
-    double centerLatitude = 22;
-    double centerLongitude = 113.72;
-    double targetLatitude = 22;
-    double targetLongitude = 113.82;
     double r = 100000;
 
     @Test
     void testsWithinRange() {
-        GeodeticCurve geoCurve = new GeodeticCalculator().calculateGeodeticCurve(
-                Ellipsoid.Sphere,
-                new GlobalCoordinates(centerLatitude, centerLongitude),
-                new GlobalCoordinates(targetLatitude, targetLongitude)
-        );
-        double distance = geoCurve.getEllipsoidalDistance();
-        System.out.println("distance:" + distance);
-        System.out.println(distance < r);
+        System.out.println(CoordinateUtil.isWithinRange(
+                10000,
+                22,
+                113.72,
+                22, 113.82));// false
+        System.out.println(CoordinateUtil.isWithinRange(
+                10000,
+                22,
+                113.72,
+                22, 113.71));// true
+
     }
 
 }
