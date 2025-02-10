2c5343d3dd588e2f7cf02c116572de911399dec7
zxd <b21032130@njupt.edu.cn>
2025-02-10 10:32:25 +0800

    add:新增测试多点距离方法

--- src/test/java/com/example/findpoints/FindPointsApplicationTests.java
+++ src/test/java/com/example/findpoints/FindPointsApplicationTests.java
@@ -1,30 +1,52 @@
 package com.example.findPoints;
 
-import org.gavaghan.geodesy.Ellipsoid;
-import org.gavaghan.geodesy.GeodeticCalculator;
-import org.gavaghan.geodesy.GeodeticCurve;
-import org.gavaghan.geodesy.GlobalCoordinates;
+import com.example.findPoints.info.PointInfo;
+import com.example.findPoints.utils.CoordinateUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
 class FindPointsApplicationTests {
-    double centerLatitude = 22;
-    double centerLongitude = 113.72;
-    double targetLatitude = 22;
-    double targetLongitude = 113.82;
     double r = 100000;
 
     @Test
     void testsWithinRange() {
-        GeodeticCurve geoCurve = new GeodeticCalculator().calculateGeodeticCurve(
-                Ellipsoid.Sphere,
-                new GlobalCoordinates(centerLatitude, centerLongitude),
-                new GlobalCoordinates(targetLatitude, targetLongitude)
-        );
-        double distance = geoCurve.getEllipsoidalDistance();
-        System.out.println("distance:" + distance);
-        System.out.println(distance < r);
+        System.out.println(CoordinateUtil.isWithinRange(
+                10000,
+                22,
+                113.72,
+                22, 113.82));// false
+        System.out.println(CoordinateUtil.isWithinRange(
+                10000,
+                22,
+                113.72,
+                22, 113.71));// true
+
+    }
+
+    @Test
+    void testsWithinRanges() {
+        List<PointInfo> pointInfos = new ArrayList<>();
+        for (int i = 0; i < 5; ++i) {
+            pointInfos.add(new PointInfo(i, (i + 1) * 2, (i + 1) * 3));
+        }
+        Map<Integer, Double> pointsMap1 = CoordinateUtil.isWithinRange(
+                10000,
+                2.1,
+                3.1,
+                pointInfos);
+        System.out.println(pointsMap1.isEmpty() ? "No Points" : pointsMap1.toString());// No Points
+        Map<Integer, Double> pointsMap2 = CoordinateUtil.isWithinRange(
+                100000,
+                2.1,
+                3.1,
+                pointInfos);
+        System.out.println(pointsMap2.isEmpty() ? "No Points" : pointsMap2.toString()); // {0=15720.304956802594}
+
     }
 
 }
